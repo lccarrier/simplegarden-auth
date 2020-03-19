e44abb8457a1cadd94a19db2dc2674eee04ef739
Louis-Charles Carrier <louis-charlescarrier@Louis-Charless-MacBook-Pro.local>
2020-03-19 17:18:58 -0400

    added Netlify URL to frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import { useIdentityContext, IdentityContextProvider } from "react-netlify-ident
 import "react-netlify-identity-widget/styles.css"
 
 function App() {
-  const url = "https://dazzling-lewin-705bfc.netlify.com"
+  const url = "https://tender-banach-f5a684.netlify.com"
   return (
     <IdentityContextProvider url={url}>
       <AuthStatusView />
